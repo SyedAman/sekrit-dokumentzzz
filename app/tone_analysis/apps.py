f07270e9480165730f433f57d3ee9a337fab2ebe
Syed Aman <sna274@nyu.edu>
2018-03-24 18:37:32 -0500

    rename the app. Also use a newer version of django

--- app/tone_analysis/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ToneAnalysisConfig(AppConfig):
-    name = 'tone_analysis'
---
+++ the_site/echo_journal/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class EchoJournalConfig(AppConfig):
+    name = 'echo_journal'
